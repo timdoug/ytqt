25a90d659dbbc93289c0d3b0d5bb05936791b148
Tim Douglas <me@timdoug.com>
2013-08-14 23:41:26 -0700

    Do versioning dynamically
    No more commits needed when youtube-dl version changes

--- make_app.py
+++ make_app.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import urllib2
+import datetime
 import tempfile
 import subprocess
 
@@ -10,6 +11,7 @@ from bs4 import BeautifulSoup
 
 temp_dir = tempfile.mkdtemp()
 youtube_dl_path = temp_dir + '/youtube-dl'
+version_path = temp_dir + '/VERSION'
 
 print 'Getting latest youtube-dl version...'
 soup = BeautifulSoup(urllib2.urlopen('http://rg3.github.io/youtube-dl/download.html').read())
@@ -20,6 +22,9 @@ print 'Downloading youtube-dl...'
 with open(youtube_dl_path, 'w') as f:
     f.write(urllib2.urlopen(youtube_dl_url).read())
 os.chmod(youtube_dl_path, 0755)
+version = datetime.datetime.now().strftime('%Y.%m.%d.%H.%M')
+with open(version_path, 'w') as f:
+    f.write(version)
 
 print 'Making Ytqt.app...'
 subprocess.Popen(' '.join([
@@ -29,13 +34,13 @@ subprocess.Popen(' '.join([
     '-p /bin/bash',
     '-i assets/ytqt.icns',
     '-u timdoug',
-    '-V `cat VERSION`',
+    '-V ' + version,
     '-I com.timdoug.ytqt',
     '-R',
     '-n "Helvetica 18"',
     '-f get_url.scpt',
     '-f ' + youtube_dl_path,
-    '-f VERSION',
+    '-f ' + version_path,
     '-l',
     '-y',
     'ytqt.sh',
